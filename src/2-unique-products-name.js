fce6bfea3ec55f5482bd3fd5ef2bfed8b5dcd247
Raissa Santos <raissa@Raissas-MacBook-Pro.local>
2023-08-21 12:20:34 -0400

    Requisito 2

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -1,7 +1,11 @@
 const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
-  // Desenvolva seu código dentro dessa função...
+  let getUniqueProductsName = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    uniqueProductsName.push(stockProducts[index].productName);
+  }
+  return uniqueProductsName;
 };
 
 module.exports = { getUniqueProductsName };
